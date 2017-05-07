543ad83429c54310987173252a82f115e0da7bc9
Benjamin Gallusser <dunking.ben@gmail.com>
2017-05-08 00:07:29 +0200

    total flop counts done

--- fast-lda/rdtsc-helper.h
+++ fast-lda/rdtsc-helper.h
@@ -20,7 +20,7 @@ typedef struct timer{
 timer start_timer(int id);
 void stop_timer(timer t);
 
-// Always adapt both enum and string array!
+// Always adapt both enum and string array (in rdtsc_helper.c)!
 enum accumulator_ids{RUN_EM, LDA_INFERENCE, DIGAMMA, LOG_SUM, LOG_GAMMA, DOC_E_STEP, LIKELIHOOD, EM_CONVERGE, INFERENCE_CONVERGE, N_ACCUMULATORS};
 extern char* accumulator_names[N_ACCUMULATORS];
 
