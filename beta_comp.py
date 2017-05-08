b576071e6770e6614a4b26b5314d25a1189dfe41
Frederic Lafrance <lafrancef@gmail.com>
2017-05-08 19:54:03 +0200

    Automation script

--- beta_comp.py
+++ beta_comp.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-# usage: python beta_comp.py <beta file> 
+# usage: python beta_comp.py <beta file>
 #
 # <beta file> is output from the lda-c code
 
@@ -26,19 +26,21 @@ def compare(f1, f2):
     topics2 = f2.readlines()
     if len(topics1) != len(topics2):
         print("Number of topics not equal!!, file 1 has %d, file 2 %d\n" % (len(topics1), len(topics2)))
-        return
+        return False
     for i in range(len(topics1)):
-        print('checking topic %2d' %(i))
         topic1 = [float(x) for x in topics1[i].split()]
         topic2 = [float(x) for x in topics2[i].split()]
         if len(topic1) != len(topic2):
-            print("Number of words not equal!!, file 1 has %d, file 2 %d\n" % (len(topic1), len(topic2)))
+            print("Number of words in topic %d not equal!!, file 1 has %d, file 2 %d\n" % (i, len(topic1), len(topic2)))
+            return False
         else:
             for j in range(len(topic1)):
                 if not IsApproximatelyEqual(topic1[j], topic2[j]):
                     print("Word %d %f %f" % (j, topic1[j], topic2[j]))
                     print("Aborting topic\n")
-                    break
+                    return False
+
+    return True
 
 
 
@@ -49,4 +51,4 @@ if __name__ == '__main__':
 
     beta_file1 = sys.argv[1]
     beta_file2 = sys.argv[2]
-    compare(open(beta_file1, "r"), open(beta_file2, "r"))    
+    compare(open(beta_file1, "r"), open(beta_file2, "r"))
