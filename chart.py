d4536e940b565f3e5286627163811ad2799bc47e
Saurav Shekhar <sauravshekhar01@gmail.com>
2017-05-16 22:54:58 +0200

    chart updated

--- chart.py
+++ chart.py
@@ -4,9 +4,6 @@ import sys
 import matplotlib.pyplot as plt
 
 
-indices = { "RUN_EM" : 0, "LDA_INFERENCE" : 1, "DIGAMMA" : 2, "LOG_SUM" : 3,
-        "LOG_GAMMA" : 9, "TRIGAMMA" : 8, "DOC_E_STEP" : 4, "LIKELIHOOD" : 5, "MLE" : 6, "OPT_ALPHA" : 7}
-
 colors = { "RUN_EM" : "green", "LDA_INFERENCE" : "blue", "DIGAMMA" : "black", "LOG_SUM" : "purple",
         "LOG_GAMMA" : "purple", "TRIGAMMA" : "cyan", "DOC_E_STEP" : "orange", "LIKELIHOOD" : "red", "MLE" : "cyan", "OPT_ALPHA" : "cyan"}       
 
@@ -20,37 +17,20 @@ def read_one_output(f):
        and header[2] == 'Average count', "Output file not in proper format"
     lines = f.readlines()
 
-    tot_flops = [0.] * len(indices)
+    tot_flops = [0.] * len(fns)
     for i in range(len(lines)):
         s = lines[i].split(',')
         fn = s[0].strip()
         if 'CONVERGE' not in fn:
             if s[2].strip() is not '0':     # code inside function not called case
                 total_cnt = float(s[1])
-                idx = indices[fn]
+                idx = fns.index(fn)
                 tot_flops[idx] += total_cnt
         pass
     return tot_flops
     pass
 
-colors2 = ['r', 'b', 'g', 'y', 'cyan']
-def bar_plot(filenames):
-    width = 1 / (len(filenames) + 1.)   # width of bars
-    ind = np.arange(len(indices))
-    fig, ax = plt.subplots()
-    for i in range(len(filenames)): 
-        flops = read_one_output(open(filenames[i], "r")) 
-        rect = ax.bar(ind + i * width, flops, width, color = colors2[i])
-
-    ax.set_ylabel('flop count')
-    ax.set_title('Flop counts for different runs per group')
-    ax.set_xticks(ind)
-    ax.set_xticklabels(fns)
-    ax.set_yscale("log")
-    plt.grid(True)
-    plt.show()
-
-def stacked_bar_plot(filenames):
+def stacked_bar_plot(filenames, xticklabels=None):
     flops = []
     for f in filenames:
         flps = read_one_output(open(f, "r"))
@@ -63,12 +43,14 @@ def stacked_bar_plot(filenames):
     ind = np.arange(len(filenames))
     bottom = np.zeros(len(ind))
     fig, ax = plt.subplots()
-    p = [None] * len(indices)
-    for i in range(len(indices)):
+    p = [None] * len(fns)
+    for i in range(len(fns)):
         p[i] = ax.bar(ind, flops[i], width, bottom=bottom, color=colors[fns[i]])
         bottom = np.sum([bottom, flops[i]], axis=0)
     
     plt.legend(p, fns)
+    if xticklabels is not None:
+        ax.set_xticklabels(xticklabels)
 
     ax.set_ylabel('flop count')
     ax.set_title('Flop counts for different runs per group')
@@ -76,18 +58,41 @@ def stacked_bar_plot(filenames):
     plt.show()
 
 
+colors2 = ['red', 'blue', 'green', 'yellow', 'cyan']
+def bar_plot(filenames, legends=None):
+    width = 1 / (len(filenames) + 1.)   # width of bars
+    ind = np.arange(len(fns))
+    fig, ax = plt.subplots()
+    p = [None] * len(filenames)
+    for i in range(len(filenames)): 
+        flops = read_one_output(open(filenames[i], "r")) 
+        p[i] = ax.bar(ind + i * width, flops, width, color = colors2[i])
+
+    ax.set_ylabel('flop count')
+    ax.set_title('Flop counts for different runs per group')
+    ax.set_xticks(ind)
+    ax.set_xticklabels(fns)
+    ax.set_yscale("log")
+    if legends is not None:
+        plt.legend(p, legends)
+    ax.grid(linestyle='--', linewidth=2, axis='y')
+    plt.show()
+
 
 if __name__ == "__main__":
-    if (len(sys.argv) - 1) % 4 is not 0:
+    if len(sys.argv) <= 1 or ((len(sys.argv) - 1) % 4 is not 0):
         print("python chart.py -f|s folder1 k1 n1 <-f|s folder2 k2 n2>...")
         sys.exit(1)
     filenames = []
+    legends = []
     for i in range(1, len(sys.argv), 4):
         mode = "fast"
-        if sys.argv[i] == '-s': mode = slow
+        if sys.argv[i] == '-s': mode = 'slow'
         filename = sys.argv[i + 1] + "/" + mode + "_timings_" + sys.argv[i + 2] + "_" + sys.argv[i + 3] + ".csv"
         filenames.append(filename)
+        legend = mode + "_" + sys.argv[i + 3] + "_" + sys.argv[i + 2]
+        legends.append(legend)
         pass
 
-    #bar_plot(filenames)
-    stacked_bar_plot(filenames)
+    bar_plot(filenames, legends)
+    stacked_bar_plot(filenames, legends)
