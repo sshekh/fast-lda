de8c592c18242d73d4eba39fecc05efd01be07ac
Frederic Lafrance <lafrancef@gmail.com>
2017-05-18 00:16:15 +0200

    Tests for log_gamma_vec

--- fast-lda/test-gamma.c
+++ fast-lda/test-gamma.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void test_batch(float* vals) {
+void test_dg_batch(float* vals) {
 
     float ref[8];
     float res[8];
@@ -14,9 +14,31 @@ void test_batch(float* vals) {
     __m256 vecres = digamma_vec(vecvals);
     _mm256_storeu_ps(res, vecres);
 
+    printf("digamma\n");
     printf("actual,computed\n");
     for (int i = 0 ; i < 8 ; i++) {
         printf("%f %f\n", ref[i], res[i]);
+    }
+}
+
+void test_lg_batch(float* vals) {
+
+    float ref_h[8];
+    float ref_u[8];
+    float res[8];
+    for (int i = 0 ; i < 8 ; i ++) {
+        ref_h[i] = lgamma(vals[i]);
+        ref_u[i] = log_gamma(vals[i]);
+    }
+
+    __m256 vecvals = _mm256_loadu_ps(vals);
+    __m256 vecres = log_gamma_vec(vecvals);
+    _mm256_storeu_ps(res, vecres);
+
+    printf("log_gamma\n");
+    printf("lib,lda,computed\n");
+    for (int i = 0 ; i < 8 ; i++) {
+        printf("%f %f %f\n", ref_h[i], ref_u[i], res[i]);
     }
 }
 
@@ -28,7 +50,9 @@ int main() {
     for (int i = 0 ; i < 8 ; i++)
         vals[i] = rand() / (float) RAND_MAX * DOM;
 
-    test_batch(vals);
+    test_dg_batch(vals);
+    printf("\n");
+    test_lg_batch(vals);
 
     return 0;
 }
