a8e83a49143de62d12780b314c2871df483781fb
Benjamin Gallusser <gallussb@student.ethz.ch>
2017-05-18 17:29:26 +0200

    updated validation to word overlap

--- beta_comp.py
+++ beta_comp.py
@@ -6,6 +6,19 @@
 
 import sys
 import numpy as np
+import math
+
+def topic_intersection(p, q, top_x):
+    ref_topics = set(np.argsort(p)[::-1][:top_x])
+    opt_topics = set(np.argsort(q)[::-1][:top_x])
+    return float(len(ref_topics & opt_topics)) / top_x
+
+def klDivergence(p, q):
+    sum = 0
+    for i,log_p_i in enumerate(p):
+        if log_p_i > -100:
+            sum += math.exp(log_p_i) * (log_p_i - q[i])
+    return sum
 
 def IsApproximatelyEqual(x, y, epsilon):
     """Returns True iff y is within relative or absolute 'epsilon' of x.
@@ -22,7 +35,7 @@ def IsApproximatelyEqual(x, y, epsilon):
 
     return False
 
-def compare(f_ref, f_opt, top_x = 20, epsilon = 1e-1):
+def compare(f_ref, f_opt, top_x = 30, epsilon = 0.8):
     topics_ref = f_ref.readlines()
     topics_opt = f_opt.readlines()
     if len(topics_ref) != len(topics_opt):
@@ -35,15 +48,35 @@ def compare(f_ref, f_opt, top_x = 20, epsilon = 1e-1):
             print("Number of words in topic %d not equal!!, file 1 has %d, file 2 %d\n" % (i, len(one_topic_ref), len(one_topic_opt)))
             return False
         else:
+            overlap = topic_intersection(one_topic_ref, one_topic_opt, top_x)
+            if overlap < epsilon:
+                print("\nVALIDATION FAILED")
+                print("Overlap threshold: %f" % epsilon)
+                print("Overlap for topic %d: %f" % (i, overlap))
+                print("\nAborting validation...\n")
+                return False
+
+            # ind_ref = np.argsort(one_topic_ref)[::-1][:top_x]
+            # div = klDivergence(
+            #     [one_topic_ref[x] for x in ind_ref], 
+            #     [one_topic_opt[x] for x in ind_ref])
+            # print("KL-Div Topic " + str(i) + ": " + str(div))
+            # if div > epsilon:
+            #     print("\nVALIDATION FAILED")
+            #     print("Validation threshold: %f" % epsilon)
+            #     print("KL-Divergence for topic %d: %f" % (i, div))
+            #     print("\nAborting validation...\n")
+            #     return False
+
             # First reverse to descending order, then pick TOP X topics from the reference file, only compare on those.
-            ind_ref = np.argsort(one_topic_ref)[::-1][:top_x]
-            for j in ind_ref:
-                if not IsApproximatelyEqual(one_topic_ref[j], one_topic_opt[j], epsilon):
-                    print("\nVALIDATION FAILED")
-                    print("Validation threshold: %f" % epsilon)
-                    print("Word %d in topic %d: %f %f" % (j, i, one_topic_ref[j], one_topic_opt[j]))
-                    print("\nAborting validation...\n")
-                    return False
+            # ind_ref = np.argsort(one_topic_ref)[::-1][:top_x]
+            # for r, j in enumerate(ind_ref):
+            #     if not IsApproximatelyEqual(one_topic_ref[j], one_topic_opt[j], epsilon):
+                    # print("\nVALIDATION FAILED")
+                    # print("Validation threshold: %f" % epsilon)
+                    # print("Word #%d (rank %d) in topic %d: %f %f" % (j, r, i, one_topic_ref[j], one_topic_opt[j]))
+                    # print("\nAborting validation...\n")
+                    # return False
 
     return True
 
