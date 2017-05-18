1f90070b92b2c83357f5fbbed798c1ba3cef507d
Frederic Lafrance <lafrancef@gmail.com>
2017-05-18 16:50:14 +0200

    Small tests]

--- fast-lda/test-gamma.c
+++ fast-lda/test-gamma.c
@@ -1,8 +1,11 @@
+#define DOUBLE
+
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/*
 void test_dg_batch(float* vals) {
 
     float ref[8];
@@ -40,19 +43,54 @@ void test_lg_batch(float* vals) {
     for (int i = 0 ; i < 8 ; i++) {
         printf("%f %f %f\n", ref_h[i], ref_u[i], res[i]);
     }
-}
+}*/
+
+#ifdef DOUBLE
+    void test_hsum() {
+        double vals[4];
+        for (int i = 0 ; i < 4 ; i++)
+            vals[i] = rand() / (double) RAND_MAX * 3.0;
+
+        __m256d vvals = _mm256_loadu_pd(vals);
+        __m256d vres = hsum(vvals);
+
+        double res[4];
+        _mm256_storeu_pd(res, vres);
+
+        printf("[%f %f %f %f] -> [%f %f %f %f]\n", vals[0], vals[1], vals[2], vals[3],
+            res[0], res[1], res[2], res[3]);
+    }
+#else
+    void test_hsum() {
+        float vals[8];
+        for (int i = 0 ; i < 8 ; i++)
+            vals[i] = rand() / (float) RAND_MAX * 3.f;
+
+        __m256 vvals = _mm256_loadu_ps(vals);
+        __m256 vres = hsum(vvals);
+
+        float res[8];
+        _mm256_storeu_ps(res, vres);
+
+        printf("[%f %f %f %f %f %f %f %f] -> [%f %f %f %f %f %f %f %f]\n",
+            vals[0], vals[1], vals[2], vals[3], vals[4], vals[5], vals[6], vals[7],
+            res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7]);
+    }
+#endif
 
 int main() {
     const float DOM = 3.f;
     srand(time(NULL));
-
+    /*
     float vals[8];
     for (int i = 0 ; i < 8 ; i++)
         vals[i] = rand() / (float) RAND_MAX * DOM;
 
     test_dg_batch(vals);
     printf("\n");
-    test_lg_batch(vals);
+    test_lg_batch(vals);*/
+
+    test_hsum();
 
     return 0;
 }
