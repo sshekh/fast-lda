d13a8b3f56394e9d3414e18b09c21297a11b1540
cristesc <cristesc@student.ethz.ch>
2017-05-22 18:33:37 +0200

    Aligned malloc.

--- fast-lda/fp.h
+++ fast-lda/fp.h
@@ -6,10 +6,13 @@ single and double precision. */
 
 #include <math.h>
 #include <immintrin.h>
+#include <stdlib.h>
 
 #ifdef FLOAT
 
     #define fp_t float
+    #define malloc(a)             _mm_malloc(a, 32)
+
     #define exp expf
     #define log logf
     #define fabs fabsf
@@ -83,6 +86,8 @@ single and double precision. */
 #else
 
     #define fp_t double
+    #define malloc(a)           _mm_malloc(a, 64)
+
     #define STRIDE 4
 
     #define LEFTOVER(n, s) ((n - s) % 4)
