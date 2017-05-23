f0f080cc522109d1e7b04f9035e93325ed3e98fe
Saurav Shekhar <shekhars@student.ethz.ch>
2017-05-23 21:47:45 +0200

    test gamma update

--- fast-lda/test-gamma.c
+++ fast-lda/test-gamma.c
@@ -5,41 +5,46 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
-void test_dg_batch(float* vals) {
 
-    float ref[8];
-    float res[8];
-    for (int i = 0 ; i < 8 ; i ++)
+void test_dg_batch(double* vals) {
+
+    double ref[4];
+    double res[4];
+    for (int i = 0 ; i < 4 ; i ++)
         ref[i] = digamma(vals[i]);
 
-    __m256 vecvals = _mm256_loadu_ps(vals);
-    __m256 vecres = digamma_vec(vecvals);
-    _mm256_storeu_ps(res, vecres);
+    __m256fp vecvals = _mm256_loadu_pd(vals);
+    __m256fp vecres = digamma_vec(vecvals);
+    _mm256_storeu_pd(res, vecres);
 
     printf("digamma\n");
     printf("actual,computed\n");
-    for (int i = 0 ; i < 8 ; i++) {
-        printf("%f %f\n", ref[i], res[i]);
+    for (int i = 0 ; i < 4 ; i++) {
+        printf("%.10lf %.10lf\n", ref[i], res[i]);
     }
-}*/
+    //printf("t2vec %lf %lf %lf %lf\n", ((double*)&t2)[0], ((double*)&t2)[1], ((double*)&t2)[2], ((double*)&t2)[3] );
+    //printf("t2 = %lf\n", t2);
+}
 
-void test_lg_batch(double* vals) {
+/*
+void test_lg_batch(float* vals) {
 
-    double ref_h[4];
-    double ref_u[4];
-    double res[4];
-    for (int i = 0 ; i < 4 ; i ++) {
+    float ref_h[8];
+    float ref_u[8];
+    float res[8];
+    for (int i = 0 ; i < 8 ; i ++) {
         ref_h[i] = lgamma(vals[i]);
         ref_u[i] = log_gamma(vals[i]);
     }
 
-    __m256d vecvals = _mm256_loadu_pd(vals);
-    __m256d vecres = log_gamma_vec(vecvals);
-    _mm256_storeu_pd(res, vecres);
+    __m256 vecvals = _mm256_loadu_ps(vals);
+    __m256 vecres = log_gamma_vec(vecvals);
+    _mm256_storeu_ps(res, vecres);
 
-    for (int i = 0 ; i < 4 ; i++) {
-        printf("%f,%f,%f,%f\n", ref_h[i], ref_u[i], res[i], ref_h[i] - res[i]);
+    printf("log_gamma\n");
+    printf("lib,lda,computed\n");
+    for (int i = 0 ; i < 8 ; i++) {
+        printf("%f %f %f\n", ref_h[i], ref_u[i], res[i]);
     }
 }
 
@@ -75,21 +80,23 @@ void test_lg_batch(double* vals) {
             res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7]);
     }
 #endif
+*/
 
 int main() {
     const float DOM = 3.f;
     srand(time(NULL));
-
+    
     double vals[4];
+    for (int i = 0 ; i < 4 ; i++)
+        //vals[i] = (i + 1) * (i + 1);
+        vals[i] = rand() / (double) RAND_MAX * DOM;
 
-    printf("lib,lda,us,err\n");
-    for (int i = 0 ; i < 10 ; i++) {
-        for (int i = 0 ; i < 4 ; i++)
-            vals[i] = rand() / (double) RAND_MAX * DOM;
+    test_dg_batch(vals);
+    printf("\n");
+    // test_lg_batch(vals);
 
-        test_lg_batch(vals);
-    }
-    //test_hsum();
+    // test_hsum();
 
     return 0;
 }
+
